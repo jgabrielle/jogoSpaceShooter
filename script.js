679f44fbdfddd93bc1a7eab73784e73968a8be9d
Julia Azevedo <jgabrielle.azevedo@gmail.com>
2020-08-22 14:40:36 -0300

    criar funcao de subir da nave

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ function flyAhip (event){
 
 }
 
+//funcao de subir
+function moveUp(){
+	let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+	if(topPosition === "0px"){
+		return
+	} else{
+		let position = parseInt(topPosition);
+		position -= 50;
+		yourShip.style.top = `${position}px`;
+	}
+}
