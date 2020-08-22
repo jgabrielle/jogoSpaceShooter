cea94a0d356e557f9087488f4fc8d95cda78a645
Julia Azevedo <jgabrielle.azevedo@gmail.com>
2020-08-22 15:21:47 -0300

    criar funcao atirar e posicao de laser

--- script.js
+++ script.js
@@ -41,4 +41,24 @@ function moveDown(){
 	}
 }
 
+//funcao atirar
+function fireLaser(){
+	let laser = createLaserElement();
+	playArea.appendChild(laser);
+	moveLaser();
+}
+
+//funcao criar posicao laser
+function createLaserElement(){
+	let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+	let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+	let newLaser = document.createElement('img');
+	newLaser.scr = 'img/shoot.png';
+	newLaser.classList.add('laser');
+	newLaser.style.left = `${xPosition}px`;
+	newLaser.style.top = `${yPosition-10}px`;
+	return newLaser;
+}
+
+
 window.addEventListener('keydown', flyAhip);
